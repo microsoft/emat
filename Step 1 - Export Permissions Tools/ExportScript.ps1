5887523ab03a8cf65cb77152ea0fb281c60d6f5a
reikor0x <vincenzo.sciarra@live.com>
2017-08-07 15:06:55 +0200

    Adopted cclayton18 suggested code for SNB export
    Commented my "raw" code and added the "more elegand" cclayton18 code for
    Send On Behalf export section

--- Step 1 - Export Permissions Tools/ExportScript.ps1
+++ Step 1 - Export Permissions Tools/ExportScript.ps1
@@ -128,7 +128,8 @@ import-csv mailboxes.csv | Get-ADPermission | select Identity,User,@{Name='Acces
 import-csv mailboxes.csv | Get-MailboxPermission | select Identity,User,@{Name='AccessRight';Expression={"MP"}},ExtendedRight,FolderPath | export-csv mp.csv -encoding "unicode" -NoTypeInformation 
 
 # SendOnBeHalf permissions
-import-csv mailboxes.csv | get-mailbox | ?{$_.GrantSendOnBehalfTo} | select Identity,@{Name='User';Expression={[string]::join(";",($_.GrantSendOnBehalfTo))}},@{Name='AccessRight';Expression={"SNB"}},ExtendedRight,FolderPath | export-csv SnB.csv -encoding "unicode" -NoTypeInformation 
+# import-csv mailboxes.csv | get-mailbox | ?{$_.GrantSendOnBehalfTo} | select Identity,@{Name='User';Expression={[string]::join(";",($_.GrantSendOnBehalfTo))}},@{Name='AccessRight';Expression={"SNB"}},ExtendedRight,FolderPath | export-csv SnB.csv -encoding "unicode" -NoTypeInformation 
+import-csv mailboxes.csv | get-mailbox | ?{$_.GrantSendOnBehalfTo} | select Identity,@{Name='User';Expression={$_.GrantSendOnBehalfTo -join ";"}},@{Name='AccessRight';Expression={"SNB"}},ExtendedRight,FolderPath | export-csv SnB.csv -encoding "unicode" -NoTypeInformation
 
 
 # Get Folder Permissions (Exchange 2010+ is required)
